af75464883145f0a763994302aec6fa56ede520d
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-03-09 21:05:26 +0100

    Sign-In Form Logic (Bloc)

--- lib/application/sign_in_form/bloc/sign_in_form_bloc.dart
+++ lib/application/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -12,9 +12,75 @@ part 'sign_in_form_bloc.freezed.dart';
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   final IAuthFacade _authFacade;
 
-  SignInFormBloc(this._authFacade) : super(SignInFormState.initial()) {
-    on<SignInFormEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    yield* event.map(
+      emailChanged: (e) async* {
+        yield state.copyWith(
+          emailAddress: EmailAddress(e.email),
+          authFailureOrSuccessOption: none(),
+        );
+      },
+      passwordChanged: (e) async* {
+        yield state.copyWith(
+          password: Password(e.password),
+          authFailureOrSuccessOption: none(),
+        );
+      },
+      registerPressed: (e) async* {
+        yield* _performActionOnAuthFacadeWithEmailAndPassword(
+          _authFacade.registerWithEmailAndPassword,
+        );
+      },
+      signInPressed: (e) async* {
+        yield* _performActionOnAuthFacadeWithEmailAndPassword(
+          _authFacade.signInWithEmailAndPassword,
+        );
+      },
+      signInWithGooglePressed: (e) async* {
+        yield state.copyWith(
+          isSubmitting: true,
+          authFailureOrSuccessOption: none(),
+        );
+        final failureOrSuccess = await _authFacade.signInWithGoogle();
+        yield state.copyWith(
+          isSubmitting: false,
+          authFailureOrSuccessOption: some(failureOrSuccess),
+        );
+      },
+    );
+  }
+
+  Stream<SignInFormState> _performActionOnAuthFacadeWithEmailAndPassword(
+    Future<Either<AuthFailure, Unit>> Function({
+      required EmailAddress emailAddress,
+      required Password password,
+    })
+        forwardedCall,
+  ) async* {
+    Either<AuthFailure, Unit>? failureOrSuccess;
+    final isEmailValid = state.emailAddress.isValid();
+    final isPasswordValid = state.password.isValid();
+    if (isEmailValid && isPasswordValid) {
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailureOrSuccessOption: none(),
+      );
+      failureOrSuccess = await forwardedCall(
+        emailAddress: state.emailAddress,
+        password: state.password,
+      );
+    }
+    yield state.copyWith(
+      isSubmitting: false,
+      showErrorMessages: true,
+      // The bottom line (optionOf) equivalent is:
+      // failureOrSuccess != null ? some(failureOrSuccess) : none()
+      authFailureOrSuccessOption: optionOf(failureOrSuccess),
+    );
   }
 }
--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:ddd_practice_flutter/domain/auth/auth_failure.dart';
+import 'package:ddd_practice_flutter/domain/auth/value_objects.dart';
 
 abstract class IAuthFacade {
   // Why we don't use Future<Either<AuthFailure, void>>?
@@ -7,12 +8,12 @@ abstract class IAuthFacade {
   // Swift's Void is actually a type, so you can use it in generics. Kotlin uses Unit which is also a regular type. 
   // Thankfully, the dartz package provides us, Dart programmers, with a Unit type too!
   Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
-    required String email,
-    required String password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
-    required String email,
-    required String password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
