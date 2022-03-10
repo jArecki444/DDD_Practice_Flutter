71fd38def1ce8ccabeeb72a9d089b9cfe9c5efe9
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-03-10 11:46:03 +0100

    Firebase Auth Setup & Facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -2,12 +2,20 @@ import 'package:dartz/dartz.dart';
 import 'package:ddd_practice_flutter/domain/core/failures.dart';
 import 'package:flutter/foundation.dart';
 
+import 'errors.dart';
+
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
 
   bool isValid() => value.isRight();
+
+  ///Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() => value.fold(
+        (leftErrorMsg) => throw UnexpectedValueError(leftErrorMsg),
+        ((r) => r), // we can use just id instead
+      );
 
   @override
   bool operator ==(Object o) {
---
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -0,0 +1,78 @@
+import 'package:ddd_practice_flutter/domain/auth/auth_failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ddd_practice_flutter/domain/auth/i_auth_facade.dart';
+import 'package:ddd_practice_flutter/domain/auth/value_objects.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthFacade implements IAuthFacade {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  FirebaseAuthFacade(this._firebaseAuth, this._googleSignIn);
+
+  @override
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  }) async {
+    final emailAddressStr = emailAddress.getOrCrash();
+    final passwordStr = password.getOrCrash();
+    try {
+      _firebaseAuth.createUserWithEmailAndPassword(
+        email: emailAddressStr,
+        password: passwordStr,
+      );
+      return right(unit); // left/right -> AuthFailure/Unit
+    } on PlatformException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        return left(const AuthFailure.emailAlreadyInUse());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  }) async {
+    final emailAddressStr = emailAddress.getOrCrash();
+    final passwordStr = password.getOrCrash();
+    try {
+      _firebaseAuth.signInWithEmailAndPassword(
+        email: emailAddressStr,
+        password: passwordStr,
+      );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.code == 'wrong-password' || e.code == 'user-not-found') {
+        return left(const AuthFailure.invalidEmailAndPasswordCombination());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return left(const AuthFailure.cancelledByUser());
+      }
+      final googleAuthentication = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuthentication.accessToken,
+        idToken: googleAuthentication.idToken,
+      );
+      return await _firebaseAuth.signInWithCredential(credential).then(
+            (value) => right(unit),
+          );
+    } on PlatformException catch (e) {
+      return left(const AuthFailure.serverError());
+    }
+  }
+}
