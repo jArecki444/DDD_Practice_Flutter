37aafe6101c23a3547902723bf1a760e19696533
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-03-11 14:17:27 +0100

    Update configuration of firebase for Android devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:ddd_practice_flutter/injection.dart';
 import 'package:ddd_practice_flutter/presentation/core/app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 
-void main() {
+Future<void> main() async {
   configureInjection(Environment.prod);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const AppWidget());
 }
 
