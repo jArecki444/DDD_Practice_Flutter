8cf769aa765305b375263c92e5fffbfb77df9d92
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-03-11 09:23:39 +0100

    Move welcome widget from main.dart into app_widget.dart

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
