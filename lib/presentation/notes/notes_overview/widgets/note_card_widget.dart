1bc5d976c1c44e9f973011c09d0e1a11fa5e3b58
jArecki444 <arkadiusz.cichurski@gmail.com>
2022-03-21 14:54:24 +0100

    Delete note dialog

--- lib/presentation/notes/notes_overview/widgets/note_card_widget.dart
+++ lib/presentation/notes/notes_overview/widgets/note_card_widget.dart
@@ -1,6 +1,8 @@
+import 'package:ddd_practice_flutter/application/notes/note_actor/bloc/note_actor_bloc.dart';
 import 'package:ddd_practice_flutter/domain/notes/note.dart';
 import 'package:ddd_practice_flutter/domain/notes/todo_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kt_dart/collection.dart';
 
 class NoteCard extends StatelessWidget {
@@ -15,35 +17,74 @@ class NoteCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: note.color.getOrCrash(),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              note.body.getOrCrash(),
-              style: const TextStyle(fontSize: 18),
-            ),
-            if (note.todos.length > 0) ...[
-              const SizedBox(height: 8),
-              Wrap(
-                spacing: 8,
-                children: [
-                  ...note.todos
-                      .getOrCrash()
-                      .map(
-                        (todo) => TodoDisplay(
-                          todo: todo,
-                        ),
-                      )
-                      .iter
-                ],
+      child: InkWell(
+        onTap: () {},
+        onLongPress: () {
+          final noteActorBloc = context.read<NoteActorBloc>();
+          _showDeletionDialog(context, noteActorBloc);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                note.body.getOrCrash(),
+                style: const TextStyle(fontSize: 18),
               ),
-            ]
-          ],
+              if (note.todos.length > 0) ...[
+                const SizedBox(height: 8),
+                Wrap(
+                  spacing: 8,
+                  children: [
+                    ...note.todos
+                        .getOrCrash()
+                        .map(
+                          (todo) => TodoDisplay(
+                            todo: todo,
+                          ),
+                        )
+                        .iter
+                  ],
+                ),
+              ]
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _showDeletionDialog(BuildContext context, NoteActorBloc bloc) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Selected Note'),
+            content: Text(
+              note.body.getOrCrash(),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            actions: [
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: const Text('DELETE'),
+                onPressed: () {
+                  bloc.add(
+                    NoteActorEvent.deleted(note),
+                  );
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 }
 
@@ -65,7 +106,6 @@ class TodoDisplay extends StatelessWidget {
                 Icons.check_box_outline_blank,
                 color: Colors.grey,
               ),
-        
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
